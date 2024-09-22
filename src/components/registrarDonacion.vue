d16add999f2797834d37b8b1948833f8d15fe815
JoseXP7 <josemanuelgraterolrodriguez@gmail.com>
2024-09-22 15:29:08 -0400

    Adicion del modulo de registro de donantes

---
+++ src/components/registrarDonacion.vue
@@ -0,0 +1,170 @@
+<script setup>
+import { ref } from 'vue'
+import { useSupabase } from '../clients/supabase'
+import Swal from 'sweetalert2'
+
+const name = ref()
+const surname = ref()
+const document = ref()
+const medicine = ref()
+const amount = ref()
+const club = ref()
+
+const regDonativo = async () => {
+  try {
+    const send = {
+      nombres: name.value,
+      apellidos: surname.value,
+      ci_rif: document.value,
+      medicamento: medicine.value,
+      cantidad: amount.value,
+      tipo: club.value,
+      fecha: new Date(),
+    }
+    const { error } = await useSupabase().supabase.from('donantes').upsert(send)
+    if (error) throw error
+  } catch (error) {
+    alert(error.message)
+  } finally {
+    Swal.fire({
+      title: 'Donativo Registrado',
+      text: 'Actualizando',
+      icon: 'success',
+    })
+  }
+}
+</script>
+
+<template>
+  <div>
+    <button
+      class="btn btn-primary"
+      data-bs-toggle="modal"
+      data-bs-target="#regDonateModal"
+    >
+      <i class="bi bi-person-heart"></i> Registrar Donativo
+    </button>
+  </div>
+  <div
+    class="modal fade"
+    tabindex="-1"
+    id="regDonateModal"
+    aria-labelledby="regDonateModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="regDonateModalLabel">
+            Registro de Donativo
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <section class="row justify-content-center">
+            <div class="col-lg-12">
+              <form @submit.prevent="regDonativo">
+                <!-- 2 column grid layout with text inputs for the first and last names -->
+                <div class="row mb-4">
+                  <div class="col">
+                    <div>
+                      <label class="form-label" for="names">Nombres</label>
+                      <input
+                        type="text"
+                        id="names"
+                        class="form-control"
+                        v-model="name"
+                      />
+                    </div>
+                  </div>
+                  <div class="col">
+                    <div>
+                      <label class="form-label" for="surnames">Apellidos</label>
+                      <input
+                        type="text"
+                        id="surnames"
+                        class="form-control"
+                        v-model="surname"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <!-- CI input -->
+                <div class="row mb-4">
+                  <div class="col-5">
+                    <div>
+                      <label class="form-label" for="ci"
+                        >Cedula de Identidad / R.I.F</label
+                      >
+                      <input
+                        type="text"
+                        id="ci"
+                        class="form-control"
+                        v-model="document"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-4">
+                    <div>
+                      <label class="form-label" for="phone">Medicamento</label>
+                      <input
+                        type="text"
+                        id="medicine"
+                        class="form-control"
+                        v-model="medicine"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-2">
+                    <div>
+                      <label class="form-label" for="age">Cantidad</label>
+                      <input
+                        type="number"
+                        id="age"
+                        class="form-control"
+                        v-model="amount"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row mb-4">
+                  <div class="col-4">
+                    <div>
+                      <label for="club">Club</label>
+                      <select class="form-select" id="club" v-model="club">
+                        <option value="Hipertenso">Hipertenso</option>
+                        <option value="Cancer">Cancer</option>
+                        <option value="Diabetico">Diabetico</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </section>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Cancelar
+          </button>
+          <button type="button" class="btn btn-primary" @click="regDonativo">
+            Registrar
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/DonantesView.vue
+++ src/views/DonantesView.vue
@@ -1,5 +1,58 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue'
+import { useSupabase } from '../clients/supabase'
+import registrarDonacion from '@/components/registrarDonacion.vue'
+const { supabase } = useSupabase()
+const donantes = ref([])
+
+async function getDonantes() {
+  const { data } = await supabase.from('donantes').select()
+  donantes.value = data
+}
+
+onMounted(() => {
+  getDonantes()
+})
+</script>
 <template>
-  <div><h1>Donantes</h1></div>
+  <div class="container">
+    <section class="d-flex align-items-center">
+      <h1 class="me-auto">Donantes</h1>
+      <registrarDonacion />
+      <div class="ms-1">
+        <button class="btn btn-primary" @click="getDonantes">
+          <i class="bi bi-arrow-clockwise"></i>
+        </button>
+      </div>
+    </section>
+    <section class="table-responsive">
+      <table class="table table-sm table-bordered">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nombres</th>
+            <th>Apellidos</th>
+            <th>Medicamento Donado</th>
+            <th>Cantidad</th>
+            <th>Tipo de Medicamento</th>
+            <th>C.I / R.I.F</th>
+            <th>Fecha</th>
+          </tr>
+        </thead>
+        <tbody v-for="donante in donantes" :key="donante.id">
+          <tr>
+            <th>{{ donante.id }}</th>
+            <td>{{ donante.nombres }}</td>
+            <td>{{ donante.apellidos }}</td>
+            <td>{{ donante.medicamento }}</td>
+            <td>{{ donante.cantidad }}</td>
+            <td>{{ donante.tipo }}</td>
+            <td>{{ donante.ci_rif }}</td>
+            <td>{{ donante.fecha }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  </div>
 </template>
 <style scoped></style>
